7f9819a169dd55e3753b0d55aafd858bfba40dcc
Dimitry364 <g4779@yandex.ru>
2026-01-26 00:49:32 +0700

    ям

--- klv-pechi/src/app/head.js
+++ klv-pechi/src/app/head.js
@@ -1,58 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
-import Script from 'next/script';
 import { getOrganizationJsonLd } from '@/seo/organizationSchema';
 
 export default function Head() {
   return (
     <>
-      {/* schema.org Organization JSON-LD */}
-      <Script
-        id='org-schema'
+      <script
         type='application/ld+json'
         dangerouslySetInnerHTML={{
           __html: JSON.stringify(getOrganizationJsonLd()),
         }}
       />
-
-      {/* Яндекс метрика */}
-      <Script
-        id='yandex-metrika'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-              (function(m,e,t,r,i,k,a){
-                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  for (var j = 0; j < document.scripts.length; j++) {
-                    if (document.scripts[j].src === r) { return; }
-                  }
-                  k = e.createElement(t);
-                  a = e.getElementsByTagName(t)[0];
-                  k.async = 1;
-                  k.src = r;
-                  a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105456265', 'ym');
-
-              ym(105456265, 'init', {
-                ssr: true,
-                webvisor: true,
-                clickmap: true,
-                ecommerce: "dataLayer",
-                accurateTrackBounce: true,
-                trackLinks: true
-              });
-            `,
-        }}
-      />
-      <noscript>
-        <div>
-          <img
-            src='https://mc.yandex.ru/watch/105456265'
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt=''
-          />
-        </div>
-      </noscript>
     </>
   );
 }
--- klv-pechi/src/app/layout.js
+++ klv-pechi/src/app/layout.js
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+import Script from 'next/script';
+import './globals.css';
+
 export const metadata = {
-  // Базовая разметка. Всё остальное — в (site)/layout.js
   title: {
     default: 'Колывань-Печи',
     template: '%s — Доставка по всей России',
@@ -21,7 +24,46 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='ru'>
-      <body>{children}</body>
+      <head>
+        {/* Яндекс.Метрика */}
+        <Script
+          id='yandex-metrika'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(105456265, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+              });
+            `,
+          }}
+        />
+      </head>
+
+      <body>
+        <noscript>
+          <img
+            src='https://mc.yandex.ru/watch/105456265'
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=''
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
